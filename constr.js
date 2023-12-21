b1467731d128d00506bdf9823cb1b7522201ddc7
Darth-InVader15 <piyush.singh1315@gmail.com>
2023-12-22 01:16:57 +0530

    added new info abt constructor

--- constr.js
+++ constr.js
@@ -4,11 +4,18 @@ function Node(data, next, prev) {
     this.data = data;
     this.next = next;
     this.prev = prev;
+
+    //Objects can also have functions
+    function alertUsr(){
+        alert("You have been alerted!")
+    }
 }
 
 let node1 = new Node(0, null, null);
 let node2 = new Node(10, null, node1);
 node1.next = node2;
 
+
 let node3 = node1.next;
 console.log(node3.data);
+node1.alertUsr();   //For calling these fn, () are used
