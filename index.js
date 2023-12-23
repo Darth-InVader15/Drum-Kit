fb8cececf8091b003f3cb4926dfcbe75206bb746
Darth-InVader15 <piyush.singh1315@gmail.com>
2023-12-23 15:27:13 +0530

    key mapped with instrument

--- index.js
+++ index.js
@@ -2,63 +2,52 @@ let currSet = document.querySelectorAll(".drum");
 let n = currSet.length;
 
 for (var i = 0; i < n; i++) {
-    currSet[i].addEventListener("click", function(i) {
-        return function() {
-            // currSet[i].style.color = 'white'; // changes color of the button to white upon being clicked
-            let tone1 = new Audio('/sounds/crash.mp3');
-
-            // alt we could have done let key = this.innerHtml;
-            // switch(button)
-
-            switch (i) {
-                case 0:
-                    tone1 = new Audio('/sounds/crash.mp3');
-                    break;
-
-                case 1:
-                    tone1 = new Audio('/sounds/kick-bass.mp3');
-                    break;
-
-                case 2:
-                    tone1 = new Audio('/sounds/snare.mp3');
-                    break;
-
-                case 3:
-                    tone1 = new Audio('/sounds/tom-1.mp3');
-                    break;
-
-                case 4:
-                    tone1 = new Audio('/sounds/tom-2.mp3');
-                    break;
-
-                case 5:
-                    tone1 = new Audio('/sounds/tom-3.mp3');
-                    break;
-
-                case 6:
-                    tone1 = new Audio('/sounds/tom-4.mp3');
-                    break;
-            }
-            tone1.play();
-        };
-    }(i)); 
-    //(i) is due to the asynch behaviour of JS. if we dont do it, no matter the value of i, 
-    // the inner fn would always receive the last val
-    //which kinda defeats the entire purpose
-
-    //keyboard listener
-    //the event inside the fn is for passing the event that triggered the listener
-    document.addEventListener("keydown", function(event){
-        console.log(event.key + " key was pressed")
-
-    })
-
-
-
-
-
-
-
-    //alternatively we could just use 'this' word inside the for loop
-    //the this operator would return the current tag which is being pressed, thus this.innerhtml would give us name of the button being pressed
+    currSet[i].addEventListener("click", function() {
+        let button = this.innerHTML;
+        soundAPTx(button);
+    }); 
 }
+
+// Keyboard listener
+document.addEventListener("keydown", function(event) {
+    console.log(event.key);
+    soundAPTx(event.key);
+});
+
+let soundAPTx = function(key) {
+    let tone1; // Declare tone1 here
+
+    switch (key) {
+        case 'w':
+            tone1 = new Audio('/sounds/crash.mp3');
+            break;
+
+        case 'a':
+            tone1 = new Audio('/sounds/kick-bass.mp3');
+            break;
+
+        case 's':
+            tone1 = new Audio('/sounds/snare.mp3');
+            break;
+
+        case 'd':
+            tone1 = new Audio('/sounds/tom-1.mp3');
+            break;
+
+        case 'j':
+            tone1 = new Audio('/sounds/tom-2.mp3');
+            break;
+
+        case 'k':
+            tone1 = new Audio('/sounds/tom-3.mp3');
+            break;
+
+        case 'l':
+            tone1 = new Audio('/sounds/tom-4.mp3');
+            break;
+    }
+
+    if (tone1) {
+        tone1.play();
+    }
+};
